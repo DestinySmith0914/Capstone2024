4e4f6bb0bd77eaa827ac54bda80fa6cd40b61614
Destiny Smith <dest0914@gmail.com>
2024-08-22 14:37:14 -0500

    Finish SPA conversion

---
+++ components/footer.js
@@ -0,0 +1,7 @@
+import html from "html-literal";
+export default () => html`
+  <footer>
+    &copy; 2020
+    <h4>Destiny Smith</h4>
+  </footer>
+`;
--- index.js
+++ index.js
@@ -2,3 +2,16 @@
 document.querySelector(".fa-bars").addEventListener("click", () => {
   document.querySelector("nav > ul").classList.toggle("hidden--mobile");
 });
+
+import { footer, header, main, nav } from "./components";
+
+function render() {
+  document.querySelector("#root").innerHTML = `
+  ${header(state)}
+    ${nav(store.nav)}
+    ${main(state)}
+    ${footer()}
+  `;
+}
+
+render();
