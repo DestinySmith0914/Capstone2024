82807f4416dbebce052b333b77af9325c74988d4
Destiny Smith <dest0914@gmail.com>
2024-08-27 21:00:50 -0500

    Add API & Router hooks

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-import Navigo from "navigo";
-import { camelCase } from "lodash";
 import { footer, header, main, nav } from "./components";
 import * as store from "./store";
+import Navigo from "navigo";
+import { camelCase } from "lodash";
+import axios from "axios";
+
 
 const router = new Navigo("/");
 
@@ -20,6 +22,52 @@ function render(state = store.home) {
 // add menu toggle to bars icon in nav bar
 document.querySelector(".fa-bars").addEventListener("click", () => {
   document.querySelector("nav > ul").classList.toggle("hidden--mobile");
+});
+
+router.hooks({
+  // We pass in the `done` function to the before hook handler to allow the function to tell Navigo we are finished with the before hook.
+  // The `match` parameter is the data that is passed from Navigo to the before hook handler with details about the route being accessed.
+  // https://github.com/krasimir/navigo/blob/master/DOCUMENTATION.md#match
+  before: (done, match) => {
+    // We need to know what view we are on to know what data to fetch
+    const view = match?.data?.view ? camelCase(match.data.view) : "home";
+    // Add a switch case statement to handle multiple routes
+    switch (view) {
+      // Add a case for each view that needs data from an API
+      case "":
+        // New Axios get request utilizing already made environment variable
+        axios
+          .get(`https://fakestoreapi.com/products`)
+          .then(response => {(res=>res.json())
+            .then(json=>console.log(json))
+            // We need to store the response to the state, in the next step but in the meantime let's see what it looks like so that we know what to store from the response.
+            console.log("response", response);
+            done();
+          })
+          .catch((error) => {
+            console.log("Failed", error);
+            done();
+          });
+          break;
+      default :
+        // We must call done for all views so we include default for the views that don't have cases above.
+        done();
+        // break is not needed since it is the last condition, if you move default higher in the stack then you should add the break statement.
+    }
+  },
+  already: (match) => {
+    const view = match?.data?.view ? camelCase(match.data.view) : "home";
+
+    render(store[view]);
+  },
+  after: (match) => {
+    router.updatePageLinks();
+
+    // add menu toggle to bars icon in nav bar
+    document.querySelector(".fa-bars").addEventListener("click", () => {
+        document.querySelector("nav > ul").classList.toggle("hidden--mobile");
+    });
+  }
 });
 
 router
